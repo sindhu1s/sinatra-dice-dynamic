4e0083b7f5297f4cea05b536d0177e38aa8220fd
sindhu1s <146214337+sindhu1s@users.noreply.github.com>
2023-11-24 00:33:53 +0000

    sintara dynamic dice

--- app.rb
+++ app.rb
@@ -5,47 +5,14 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/2/6") do
+get ("/dice/:zebra/:monkey") do
+  @num_dice= params.fetch("zebra").to_i
+  @num_side= params.fetch("monkey").to_i
+
   @rolls = []
 
-  2.times do
-    die = rand(1..6)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_six)
-end
-
-get("/dice/2/10") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..10)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_ten)
-end
-
-get("/dice/1/20") do
-  @rolls = []
-
-  1.times do
-    die = rand(1..20)
-
-    @rolls.push(die)
-  end
-
-  erb(:one_twenty)
-end
-
-get("/dice/5/4") do
-  @rolls = []
-
-  5.times do
-    die = rand(1..4)
+  @num_dice.times do
+    die = rand(1..@num_side)
 
     @rolls.push(die)
   end
